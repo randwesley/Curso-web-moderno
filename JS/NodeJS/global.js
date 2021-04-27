969170d6d8ef4acba30716733f40ac387538e6bb
Randerson <randwesley0@gmail.com>
2021-04-26 22:27:40 -0300

    Explicação sobre o que é o OBJ Global

---
+++ JS/NodeJS/global.js
@@ -0,0 +1,7 @@
+//console.log(global);
+globalThis.MeuApp = Object.freeze({
+    saudacao() {
+        return 'Estou em todos os lugares'
+    },
+    nome: 'Sistema Legal'
+})
---
+++ JS/NodeJS/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MeuApp.saudacao());
+
+MeuApp.nome ='Eita!'
+console.log(MeuApp.nome);
